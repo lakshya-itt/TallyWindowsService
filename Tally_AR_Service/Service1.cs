067b6ad73ffab850367c1eb390cf6d5de1610e03
Lakshya Jain <lakshya.jain@intimetec.com>
2020-04-08 10:37:02 +0530

    Add project files.

---
+++ Tally_AR_Service/Service1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tally_AR_Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        private readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void onDebug()
+        {
+            log.Debug("Debugging started");
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("Execution Started");
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("Execution Stopped");
+        }   
+    }
+}
